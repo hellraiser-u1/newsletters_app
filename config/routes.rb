e18fb6ccf51afb64fd16277c4fc53f53fb3b33dd
Johan van Rensburg <hellraiser.u1@gmail.com>
2012-05-08 22:56:27 +0200

    Finished layouts and routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,5 @@
 NewslettersApp::Application.routes.draw do
-
-  #get "static_pages/home"
-  #get "static_pages/help"
-  #get "static_pages/about"
-  #get "static_pages/contact"
+  get "users/new"
 
   root to: 'static_pages#home'
 
@@ -11,6 +7,7 @@ NewslettersApp::Application.routes.draw do
   match '/about',   to: 'static_pages#about'
   match '/contact', to: 'static_pages#contact'
 
+  match '/signup',  to: 'users#new'
   
   resources :subscriptions
   resources :newsletters
