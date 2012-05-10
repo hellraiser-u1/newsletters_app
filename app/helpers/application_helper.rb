8d9b4f8cf316ea2ea3aca67c4b2bd3115c61aa05
Johan van Rensburg <hellraiser.u1@gmail.com>
2012-05-10 16:30:11 +0200

    Commit

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,7 +1,7 @@
 module ApplicationHelper
   # Returns the full title on a per-page basis.
   def full_title(page_title)
-    base_title = "Searle Consulting"
+    base_title = "Newsletter Application"
     if page_title.empty?
       base_title
     else
